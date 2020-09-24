5b7a65b677f2f638f9c141b64097e91cfdd3f764
Tom LaMarre <tlamarre91+github@gmail.com>
2020-09-24 17:47:17 -0500

    modified:   public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -51711,7 +51711,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/home/tom/src/aoe2-demo/resources/sass/app.scss'\n    at /home/tom/src/aoe2-demo/node_modules/webpack/lib/NormalModule.js:316:20\n    at /home/tom/src/aoe2-demo/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/tom/src/aoe2-demo/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at /home/tom/src/aoe2-demo/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:75:11)");
 
 /***/ }),
 
